2bf7a4b97b1cc459402f9372070afe007ad8a90b
maltomare70 <maltomare70@gmail.com>
2013-07-24 00:39:14 +0200

    .

---
+++ TeamSalesForAndroid/obj/Debug/android/src/teamsalesforandroid/Tools.java
@@ -0,0 +1,46 @@
+package teamsalesforandroid;
+
+
+public class Tools
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TeamSalesForAndroid.Tools, TeamSalesForAndroid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Tools.class, __md_methods);
+	}
+
+
+	public Tools ()
+	{
+		super ();
+		if (getClass () == Tools.class)
+			mono.android.TypeManager.Activate ("TeamSalesForAndroid.Tools, TeamSalesForAndroid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
